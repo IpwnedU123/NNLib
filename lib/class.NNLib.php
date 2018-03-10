daba138709847505f13668c314e12d66c5b9f948
Steph Lockhomes <antherynx.studio@mail.com>
2018-03-10 18:18:49 -0500

    Now you can create accounts!

--- lib/class.NNLib.php
+++ lib/class.NNLib.php
@@ -5,16 +5,28 @@
  */
 
 class NNLib {
+    // NNLib config
     private $config;
 
+    // NNLib constructor
     function __construct() {
+        // Include config
         include(__DIR__ . "/config.NNLib.php");
+
+        // Set config data
         $this->config = $config;
     }
 
+    /*
+     * validateUsername([USERNAME(STRING)]);
+     * Description: Validates a username
+     * Return: [true|false]
+     */
 	function validateUsername($username) {
+        // Init cURL
     	$ch = curl_init();
 
+        // Set cURL Options
     	curl_setopt_array($ch, array(
     		CURLOPT_SSL_VERIFYPEER => 0,
     	    CURLOPT_RETURNTRANSFER => true,
@@ -25,14 +37,23 @@ class NNLib {
         	)
     	));
 
+        // Set cURL request URL
     	curl_setopt($ch, CURLOPT_URL, $this->config["apiEndpoint"] . "people/" . $username);
 
+        // Check if empty and execute
     	return empty(curl_exec($ch));
 	}
 
+    /*
+     * validateEmail([EMAIL(STRING)]);
+     * Description: Validates a email
+     * Return: [true|false]
+     */
 	function validateEmail($email) {
+        // Init cURL
     	$ch = curl_init();
 
+        // Set cURL Options
     	curl_setopt_array($ch, array(
     		CURLOPT_SSL_VERIFYPEER => 0,
     	    CURLOPT_RETURNTRANSFER => true,
@@ -46,8 +67,103 @@ class NNLib {
         	)
     	));
 
+        // Set cURL request URL
     	curl_setopt($ch, CURLOPT_URL, $this->config["apiEndpoint"] . "support/validate/email");
 
+        // Check if empty and execute
     	return empty(curl_exec($ch));
 	}
+
+    /*
+     * createAccount([DATA(ARRAY)]);
+     * Description: Creates an account
+     * Return: [Account PID|false]
+     */
+    function createAccount($data) {
+        $xml = new SimpleXMLElement("<?xml version=\"1.0\" encoding=\"UTF-8\"?><person></person>");
+
+        // Main data
+        $xml->addChild("birth_date", $data["birth_date"]);
+        $xml->addChild("user_id", $data["user_id"]);
+        $xml->addChild("password", $data["password"]);
+        $xml->addChild("country", $data["country"]);
+        $xml->addChild("language", $data["language"]);
+        $xml->addChild("tz_name", $data["tz_name"]);
+
+        // Agreement
+        $agreement = $xml->addChild("agreement");
+        $agreement->addChild("agreement_date", $data["agreement"]["agreement_date"]);
+        $agreement->addChild("country", $data["agreement"]["country"]);
+        $agreement->addChild("location", $data["agreement"]["location"]);
+        $agreement->addChild("type", $data["agreement"]["type"]);
+        $agreement->addChild("version", $data["agreement"]["version"]);
+
+        // Email
+        $email = $xml->addChild("email");
+        $email->addChild("address", $data["email"]["address"]);
+        $email->addChild("owned", "N");
+        $email->addChild("parent", "N");
+        $email->addChild("primary", "Y");
+        $email->addChild("type", "DEFAULT");
+        $email->addChild("validated", "N");
+
+        // Mii
+        $mii = $xml->addChild("mii");
+        $mii->addChild("name", $data["mii"]["name"]);
+        $mii->addChild("primary", "Y");
+        $mii->addChild("data", $data["mii"]["data"]);
+
+        // Parental consent
+        // This isn't really implemented yet.
+        $parental_consent = $xml->addChild("parental_consent");
+        $parental_consent->addChild("scope", 0);
+        $parental_consent->addChild("consent_date", $data["parental_consent"]["consent_date"]);
+        $parental_consent->addChild("approval_id", 0);
+
+        // Main data
+        $xml->addChild("gender", $data["gender"]);
+        $xml->addChild("region", $data["region"]);
+        $xml->addChild("marketing_flag", "Y");
+        $xml->addChild("off_device_flag", "Y");
+
+        // Init cURL
+        $ch = curl_init();
+
+        // Set cURL Options
+        curl_setopt_array($ch, array(
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSLCERT => $this->config["sslCert"],
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => $xml->asXML(),
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: application/xml",
+                "X-Nintendo-Platform-ID: " . $this->config["device"]["platformId"],
+                "X-Nintendo-Device-Type: " . $this->config["device"]["deviceType"],
+                "X-Nintendo-Device-ID: " . $this->config["device"]["deviceId"],
+                "X-Nintendo-Serial-Number: " . $this->config["device"]["serialNumber"],
+                "X-Nintendo-System-Version: " . $this->config["device"]["systemVersion"],
+                "X-Nintendo-Region: " . $this->config["device"]["region"],
+                "X-Nintendo-Country: " . $this->config["device"]["country"],
+                "X-Nintendo-Environment: " . $this->config["device"]["environment"],
+                "X-Nintendo-Device-Cert: " . $this->config["device"]["deviceCert"],
+                "X-Nintendo-Client-ID: " . $this->config["clientId"],
+                "X-Nintendo-Client-Secret: " . $this->config["clientSecret"]
+            )
+        ));
+
+        // Set cURL request URL
+        curl_setopt($ch, CURLOPT_URL, $this->config["apiEndpoint"] . "people/");
+
+        // Execute and convert to XML element
+        $person = new SimpleXMLElement(curl_exec($ch));
+
+        // Check if PID doesn't exist in data
+        if (!$person->pid) {
+            return false;
+        }
+
+        // Else return PID
+        return $person->pid;
+    }
 }
--- test.NNLib.php
+++ test.NNLib.php
@@ -2,14 +2,61 @@
 require_once("lib/class.NNLib.php");
 
 $nnlib = new NNLib();
-if ($nnlib->validateUsername("thisisatest")) {
-	echo "This username is valid.<br>";
-} else {
-	echo "This username is invalid!<br>";
+
+$data = array(
+	"birth_date" => "1990-01-01",
+	"user_id" => "<YOUR_USERNAME>",
+	"password" => "<YOUR_PASSWORD>",
+	"country" => "US",
+	"language" => "en",
+	"tz_name" => "America/New_York",
+	"agreement" => array(
+		"agreement_date" => "1990-01-01T01:01:01",
+		"country" => "US",
+		"location" => "https://account.nintendo.net/v1/api/people/content/agreements/Nintendo-Network-EULA/0300",
+		"type" => "NINTENDO-NETWORK-EULA",
+		"version" => "0300"
+	),
+	"email" => array(
+		"address" => "<YOUR_EMAIL>"
+	),
+	"mii" => array(
+		"name" => "TestMii",
+		"data" => "AwAAQEuX4+nGhzGy04Y5PxShgom0rwAAIkBwAHIAbwBkAHQAZQBzAHQAMQAAAEBAAAAhAQJoRBgmNEYUgRIXaA0AACkAUkhQSQBhAG4AAAAAAAAAAAAAAAAAAAAAAN2k"
+	),
+	"parental_consent" => array(
+		"consent_date" => "1990-01-01T01:01:01"
+	),
+	"gender" => "M",
+	"region" => 0
+);
+
+$res = createNNID($data, $nnlib);
+switch ($res) {
+	case "username":
+	echo "Username is invalid.";
+	break;
+	case "email":
+	echo "Email is invalid.";
+	break;
+	default:
+	echo "Success! NNID was created successfully! Account PID: " . $res;
+	break;
 }
 
-if ($nnlib->validateEmail("thisisatest@gmail.com")) {
-	echo "This email is valid.<br>";
-} else {
-	echo "This email is invalid!<br>";
+function createNNID($data, $nnlib) {
+	if (!$nnlib->validateUsername($data["user_id"])) {
+		return "username";
+	}
+
+	if (!$nnlib->validateEmail($data["email"]["address"])) {
+		return "email";
+	}
+
+	$response = $nnlib->createAccount($data);
+	if ($response) {
+		return $response;
+	} else {
+		return "account";
+	}
 }
